d07f44b399923151a378083d4625aba79f218668
Cuong Dang <70055475+Amecara@users.noreply.github.com.>
2025-05-12 10:11:51 +0700

    Finish ex4.3-4.7

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./utils/config');
-const logger = require('./utils/logger');
-const middleware = require('./utils/middleware');
-const blogsRouter = require('./controllers/blogs');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+const blogsRouter = require("./controllers/blogs");
 
 const app = express();
 app.use(cors());
 
-logger.info('connecting to ', config.MONGODB_URI);
+logger.info("connecting to ", config.MONGODB_URI);
 
 mongoose.connect(config.MONGODB_URI)
-    .then(() => logger.info('connected to MongoDB'))
-    .catch(error => logger.error('error connecting to MongoDB: ', error.message));
+  .then(() => logger.info("connected to MongoDB"))
+  .catch((error) =>
+    logger.error("error connecting to MongoDB: ", error.message)
+  );
 
 // use to load static files (images etc.)
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
 app.use(express.json());
 
 // /api/blogs will automatically redirect to blogsRouter
-app.use('/api/blogs', blogsRouter);
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
